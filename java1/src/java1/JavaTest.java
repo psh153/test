8dd881adb3ad6aeb1ea98cc8bfa07bfe256ac9fd
psh153 <parkseunghun@localhost>
2025-06-28 12:21:23 +0900

    커

--- java1/src/java1/JavaTest.java
+++ java1/src/java1/JavaTest.java
@@ -1,13 +1,15 @@
 package java1;
 
-public class JavaTest {
+import java.util.Arrays;
 
+public class JavaTest {
 	public static void main(String[] args) {
-		int a = 100;
-		System.out.println(a);
-		//test12345
-		//패치 테스트
-		// 패치 2123
+		int[] pwArr = new int[4];
+		
+		for(int i = 0; i < pwArr.length; i++) {
+			pwArr[i] = (int)(Math.random()*9);
+		}
+		System.out.println(Arrays.toString(pwArr)); // z커밋테스트
 	}
 
 }
